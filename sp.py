c9467f4d3ec364053d2bb03362ab34d2ff3eb33c
Henrique Santos <silvashrq@gmail.com>
2025-04-22 20:28:11 -0300

    Deu boa

--- sp.py
+++ sp.py
@@ -1,27 +1,51 @@
+import speech_recognition as sr
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-import os
+import pyttsx3
+import os 
+
+listener = sr.Recognizer()
+
+sr.Microphone.list_microphone_names()
+
+def ouvir():
+    try:
+        with sr.Microphone(device_index=1) as source:
+            print('Listening...')
 
 # Configurações de autenticação
 os.environ['SPOTIPY_CLIENT_ID'] = '631c53df63f841d2962c9c06d746a5c6'
 os.environ['SPOTIPY_CLIENT_SECRET'] = '4ddee1ce1dd646ce841e979d030590e6'
 os.environ['SPOTIPY_REDIRECT_URI'] = 'https://example.com/retorno'
 
-# Permissões necessárias para controlar a reprodução
-scope = "user-read-playback-state user-modify-playback-state"
+scope = "user-read-playback-state,user-modify-playback-state"
+sp = spotipy.Spotify(client_credentials_manager=SpotifyOAuth(scope=scope))
 
-# Autenticação correta para controle de reprodução
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+engine = pyttsx3.init()
+engine.say('Hello')
+engine.runAndWait()
 
-# Música a ser tocada
-query = 'Runaway'
-results = sp.search(query, 1, 0, "track")
+while True:
+    command = ouvir()
 
-# Verifica se encontrou a música
-if results["tracks"]["items"]:
-    track_uri = results["tracks"]["items"][0]["uri"]
-    sp.start_playback(uris=[track_uri])
-    print("🎵 Música tocando:", results["tracks"]["items"][0]["name"])
-else:
-    print("❌ Nenhuma música encontrada.")
+    if 'spotify play' in command.lower():
+        query = command.lower().replace('spotify play','').strip()
 
+        results = sp.search(query,1,0,"track")
+
+        nome_artista = results['tracks']['items'][0]['artists'][0]['name']
+        nome_musica = results['tracks']['items'][0]['name']
+        track_uri = results['tracks']['items'][0]['uri']
+
+        engine.say(f'Playing {nome_musica} by {nome_artista}')
+        engine.runAndWait()
+
+        sp.start_playback(uris=[track_uri])
+
+    elif 'spotify pause' in command.lower():
+        sp.pause_playback()
+    elif 'spotify continue' in command.lower():
+        sp.start_playback()
+    elif 'spotify change volume to' in command.lower():
+        volume = int(command.lower().replace('spotify change volume to','').strip())
+        sp.volume(volume)
